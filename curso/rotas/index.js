f55c797692f9149116ace5a3066c76601af16f6d
Arcanjo <rafaelarcanjo129@hotmail.com>
2021-10-04 22:06:23 -0300

    Entendendo os parametros

--- curso/rotas/index.js
+++ curso/rotas/index.js
@@ -2,16 +2,20 @@ const express = require("express"); // retorna a função que cria o expree
 const { listenerCount } = require("process");
 const app = express(); // recebe a funçao do express
 
-app.get("/", function(req, res){  // cria uma rota para a aplicação
+app.get('/', function(req, res){  // cria uma rota para a aplicação
     res.send("Seja bem-vindo ao meu app")  
 })
 
-app.get("/sobre", function(req,res){
+app.get('/sobre', function(req,res){
     res.send("Minha pagina sobre")
 })
 
-app.get("/blog", function(req,res){
+app.get('/blog', function(req,res){
     res.send("Bem-vindo ao meu blog")
+})
+
+app.get('/ola/:nome/:cargo/:cor', function(req,res){  // para criar parametros usa o sinal de :
+    res.send("<h1>ola " + req.params.nome + '</h1>' +"<h2>Seu cargo é: " + req.params.cargo + '</h2>' + "<h3>A cor escolhida foi: " + req.params.cor + '</h3>'); // a função send so pode ser enviada 1 vez na funçao
 })
 
 
