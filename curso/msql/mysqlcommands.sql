1a8bec685b82c4787f06be083f3d829365679c89
Arcanjo <rafaelarcanjo129@hotmail.com>
2021-10-15 05:08:19 -0300

    Separando pastas

--- curso/msql/mysqlcommands.sql
+++ curso/msql/mysqlcommands.sql
@@ -11,12 +11,22 @@ use sistemadecadastro; // entra no banco de dados selecionado.
 
 show tables; // mostra as tabelas.
 
-insert into usuarios() values(); // insere dados na tavela usuarios.
+create table usuarios(
+    nome VARCHAR(50),
+    email VARCHAR(100),
+    idade INT
+) // cria a tabela 
+
+insert into usuarios(nome, email, idade) values(
+    "Rafael",
+    "Rafaelarcanjo@gmail.com",
+    20
+); // insere dados na tavela usuarios.
 
 delete from usuarios where idade = 8; // deleta da tabela usuarios todos os dados ondeidade for igual a 8. 
 
-select * from usuarios; // mostra atabela usuarios inteira
+select * from usuarios; // mostra atabela usuarios inteira.
 
 select * from usuarios where idade = 8; // mostrar na tabela usuarios quem tem idade iguala 8.
 
-update usuarios set nome = "Nome de teste" where nome = "Rafael Arcanjo"; // altera dadosna tabela.
+update usuarios set nome = "Nome de teste" where nome = "Rafael Arcanjo"; // altera dados na tabela.
